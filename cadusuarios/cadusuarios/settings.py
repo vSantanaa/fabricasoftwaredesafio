aac1ca65b53fefa430b4844c3b30150d74efd89a
André Santana <andrevssantana@gmail.com>
2024-08-26 09:33:04 -0300

    Primeira atualização
    Integrando banco de dados postgres

--- cadusuarios/cadusuarios/settings.py
+++ cadusuarios/cadusuarios/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'cadusuarios.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Cadusuarios',
+        'USER': 'postgres',
+        'PASSWORD': '0000',
+        'HOST': 'localhost',
     }
 }
 
